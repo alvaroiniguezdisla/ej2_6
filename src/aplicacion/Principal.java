e5fa6079bfe2085aa654c340881866e08f3a160c
Álvaro <ainiguezdislaa@gmail.com>
2023-02-20 15:23:03 +0100

    ej6 terminado

--- src/aplicacion/Principal.java
+++ src/aplicacion/Principal.java
@@ -1,2 +1,13 @@
-package aplicacion;public class Principal {
+package aplicacion;
+import dominio.ej2_6;
+
+public class Principal {
+    public static void main(String[] args) {
+        double [] lista= {1,2,3,4,5};
+
+        System.out.println(ej2_6.desviacion(lista));
+
+
+
+    }
 }
--- src/dominio/ej2_6.java
+++ src/dominio/ej2_6.java
@@ -1,2 +1,39 @@
-package dominio;public class ej2_6 {
+package dominio;
+
+public class ej2_6 {
+
+    public static double media(double[]lista,int i) {
+
+
+        if(i==0) {
+            return lista[i];
+        }else {
+            return lista[i]+ media(lista,i-1);
+        }
+
+    }
+
+    public static double cuadrados(double[]lista,int i) {
+        if(i==0) {
+            return lista[i]*lista[i];
+        }else {
+            return lista[i]*lista[i]+cuadrados(lista,i-1);
+        }
+    }
+    public static double desviacion(double[]lista) {
+        double media=media(lista,lista.length-1)/lista.length;
+        double cuadrados=cuadrados(lista,lista.length-1);
+        double varianza=(cuadrados/lista.length)-(media*media);
+        double desviacion=Math.sqrt(varianza);
+        return  desviacion;
+    }
+
+
+
+
+
+
+
+
 }
+
